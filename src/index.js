9cc3a0e14350b380d4dc0153c0bbf6889aa45ffb
Henry Kaufman <hkaufman19@gmail.com>
2017-09-02 15:39:17 -0600

    :kiwi_fruit: fixed main file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as Base } from './Base'
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as Container } from './Container'
+export { default as Heading } from './Heading'
+export { default as Input } from './Input'
+export { default as Link } from './Link'
+export { default as Text } from './Text'
